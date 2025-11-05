5df065d453409a7716428d776454be9632e86584
y-sakurai <235877267+y-sakurai-code@users.noreply.github.com>
2025-11-05 13:43:29 +0000

    rename controller name

--- db/migrate/20251104114412_create_lists.rb
+++ db/migrate/20251104114412_create_lists.rb
@@ -1,6 +1,6 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
-    create_table :books do |t|
+    create_table :lists do |t|
 
       t.string :title
       t.string :body
---
+++ db/migrate/20251105131452_renamelists_tobooks.rb
@@ -0,0 +1,4 @@
+class RenamelistsTobooks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
